5f07de3019f0d1817a77b77698e6d24b9ddbb0c7
Kevin Michael Schott <k.schott95@gmail.com>
2022-02-04 10:46:41 +0100

    cleanup; better formatting

--- php/complementaryDNA.php
+++ php/complementaryDNA.php
@@ -1,21 +1,25 @@
 <html lang="en">
 <body>
-Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
+<small>
+    Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
 
-If you want to know more: http://en.wikipedia.org/wiki/DNA
+    If you want to know more: http://en.wikipedia.org/wiki/DNA
 
-In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". You function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+    In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". You function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
 
-More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
+    More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
 
-Example: (input --> output)
+    Example: (input --> output)
 
-"ATTGC" --> "TAACG"
-"GTAT" --> "CATA"
-dnaStrand []        `shouldBe` []
-dnaStrand [A,T,G,C] `shouldBe` [T,A,C,G]
-dnaStrand [G,T,A,T] `shouldBe` [C,A,T,A]
-dnaStrand [A,A,A,A] `shouldBe` [T,T,T,T]
+    "ATTGC" --> "TAACG"
+    "GTAT" --> "CATA"
+    dnaStrand []        `shouldBe` []
+    dnaStrand [A,T,G,C] `shouldBe` [T,A,C,G]
+    dnaStrand [G,T,A,T] `shouldBe` [C,A,T,A]
+    dnaStrand [A,A,A,A] `shouldBe` [T,T,T,T]
+</small>
+<br>
+<br>
 
 <?php
 function DNA_strand($dna) {
--- php/persistentBugger.php
+++ php/persistentBugger.php
@@ -1,11 +1,15 @@
 <html lang="en">
 <body>
-Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+<small>
+    Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
 
-For example (Input --> Output):
-39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
-999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
-4 --> 0 (because 4 is already a one-digit number)
+    For example (Input --> Output):
+    39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
+    999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
+    4 --> 0 (because 4 is already a one-digit number)
+</small>
+<br>
+<br>
 
 <?php
 function persistence(int $num): int {
