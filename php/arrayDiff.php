0320b240a7615ce97594e60c113ed16e914054a8
Kevin Michael Schott <k.schott95@gmail.com>
2022-02-04 12:13:28 +0100

    Solved: Array diff of $a and $b

---
+++ php/arrayDiff.php
@@ -0,0 +1,25 @@
+<html lang="en">
+<body>
+<small>
+    Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+
+    It should remove all values from list a, which are present in list b keeping their order.
+
+    arrayDiff([1,2],[1]) == [2]
+    If a value is present in b, all of its occurrences must be removed from the other:
+
+    arrayDiff([1,2,2,2,3],[2]) == [1,3]
+</small>
+<br>
+<br>
+
+<?php
+function arrayDiff($a, $b) {
+//    return array_values(array_diff($a, $b));
+    return array_values(array_filter($a, function ($var) use ($b) {
+        return !in_array($var, $b);
+    }));
+}
+?>
+</body>
+</html>
