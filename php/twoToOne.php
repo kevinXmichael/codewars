3548c5023f214916bf1d727d361507c162a75d1b
Kevin Michael Schott <k.schott95@gmail.com>
2022-02-04 10:59:03 +0100

    Solved: Take 2 strings and combine them into a unique one (sorted)

--- php/twoToOne.php
+++ php/twoToOne.php
@@ -1,7 +1,7 @@
 <html lang="en">
 <body>
 <small>
-    Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+    Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
 
     Examples:
     a = "xyaabbbccccdefww"
@@ -15,7 +15,14 @@
 <br>
 
 <?php
+function longest($a, $b) {
+    $split = array_unique(str_split( $a . $b));
+    sort($split);
+    return implode($split);
+}
 
+$longest = longest("xyaabbbccccdefww", "xxxxyyyyabklmopq");
+echo strcmp($longest, "abcdefklmopqwxy");
 ?>
 </body>
 </html>
