fb9a7a9ee4354b578a64009daa5c312f0f2f15af
Kevin Michael Schott <k.schott95@gmail.com>
2022-02-08 10:25:20 +0100

    Solved: mexican wave

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,9 +2,10 @@
 // mod create_phone_num;
 // mod digital_root;
 // mod largest_five_digit_number;
-mod get_middle;
+// mod get_middle;
+mod mexican_wave;
 
 fn main() {
-    let result = get_middle::get_middle("test");
-    println!("The result is: {}", result);
+    let result = mexican_wave::wave("hello");
+    println!("The result is: {:?}", result);
 }
