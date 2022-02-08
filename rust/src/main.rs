361e720d317c841218468667f832dea2837be02d
Kevin Michael Schott <k.schott95@gmail.com>
2022-02-08 10:33:25 +0100

    Solved: odd / even

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,9 +3,10 @@
 // mod digital_root;
 // mod largest_five_digit_number;
 // mod get_middle;
-mod mexican_wave;
+// mod mexican_wave;
+mod odd_or_even;
 
 fn main() {
-    let result = mexican_wave::wave("hello");
-    println!("The result is: {:?}", result);
+    let result = odd_or_even::odd_or_even(vec![1, 2, 3]);
+    println!("The result is: {}", result);
 }
---
+++ rust/src/odd_or_even.rs
@@ -0,0 +1,20 @@
+pub fn odd_or_even(numbers: Vec<i32>) -> String {
+    // (if numbers.iter().sum::<i32>() % 2 == 0 {"even"} else {"odd"}).to_string()
+    if numbers.into_iter().sum::<i32>() % 2 == 0 { String::from("even") } else { String::from("odd") }
+}
+
+// Given a list of integers, determine whether the sum of its elements is odd or even.
+
+// Give your answer as a string matching "odd" or "even".
+
+// If the input array is empty consider it as: [0] (array with a zero).
+
+// Examples:
+// Input: [0]
+// Output: "even"
+
+// Input: [0, 1, 4]
+// Output: "odd"
+
+// Input: [0, -1, -5]
+// Output: "even"
