09232943e669aa5010b171264e7172aa310a58e8
Kevin Michael Schott <k.schott95@gmail.com>
2022-02-11 21:22:19 +0100

    Solved: Disemvowel Trolls

---
+++ shell/disemvowel_trolls.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Return a string (passed as first argument $1) without vowels:
+
+echo $1 | sed 's/[aeiouAEIOU]//g'
+
+
+
+# Disemvowel Trolls: Trolls are attacking your comment section!
+# A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+# Your task is to write a function that takes a string and return a new string with all vowels removed.
+# For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+# Note: for this kata y isn't considered a vowel.
