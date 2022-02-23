7b0284894a6ad34ad0c4455b42c8b487e6d1bf93
Kevin Michael Schott <k.schott95@gmail.com>
2022-02-23 16:55:27 +0100

    working on max/min numbers of array in rust

---
+++ rust/src/highest_and_lowest.rs
@@ -0,0 +1,7 @@
+pub fn high_and_low(numbers: &str) -> String {
+  let numbers_vec = vec!(numbers.split(" "));
+  print!("{:?}", numbers_vec);
+  print!("len = {}", numbers_vec.len());
+//   print!("{}", &numbers_vec[0]);
+  return String::from("");
+}
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,9 +1,9 @@
 // mod test;
-mod weight_for_weight;
+mod highest_and_lowest;
 
 fn main() {
     // test::run();
 
-    let result = weight_for_weight::order_weight("103 123 4444 99 2000");
-    println!("The result is: {} === {}?", result, "2000 103 123 4444 99");
+    let result = highest_and_lowest::high_and_low("8 3 -5 42 -1 0 0 -9 4 7 4 -4");
+    println!("The result is: {} === {}?", result, "42 -9");
 }
